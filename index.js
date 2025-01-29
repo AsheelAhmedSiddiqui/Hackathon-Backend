9b1d95ebe7d8b45e378993e72eaf1bc5bc8b8312
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2025-01-30 03:05:05 +0500

    add vercel url in cors

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ dotenv.config({ path: ".env.local" });
 
 app.use(express.json());
 
-
 app.use(
 	cors({
-	  origin: [
-		"http://localhost:5173",
-		"https://hackathon-backend-production-2663.up.railway.app/",
-	  ],
-	  methods: ["GET", "POST", "PUT", "DELETE"],
-	  allowedHeaders: ["Content-Type", "Authorization"],
-	  credentials: true, // Required for cookies
+		origin: [
+			"http://localhost:5173",
+			"https://hackathon-backend-production-2663.up.railway.app/",
+			"https://hackathon-frontend-ia0xs5gk9-asheelahmedsiddiquis-projects.vercel.app",
+		],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true, // Required for cookies
 	})
-  );
+);
 
 connectDB();
 
@@ -35,8 +35,6 @@ app.get("/", (req, res) => {
 app.use("/users", usersRoutes);
 app.use("/categories", categoriesRoutes);
 app.use("/subcategories", subcategoriesRoutes);
-
-
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
